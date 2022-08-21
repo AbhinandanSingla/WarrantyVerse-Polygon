a5a9a71b54ae2befc853f7b3f52be5ff7f94c824
Deepu Singla <52960031+AbhinandanSingla@users.noreply.github.com>
2022-08-21 16:57:00 +0530

    README.md

--- src/hooks/useMetaMask.js
+++ src/hooks/useMetaMask.js
@@ -29,7 +29,7 @@ export const MetaMaskProvider = ({children}) => {
         }
     };
     useEffect(() => {
-        checkMetamaskAvailability().then(v => v);
+        // checkMetamaskAvailability().then(v => v);
     }, [])
     const values = useMemo(() => ({
         accountAddress,
